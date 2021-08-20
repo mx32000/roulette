d1966bce533131f2e9f387f974e40c780b2192bf
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-20 14:12:24 -0500

    setting up navbar links and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <header><h1>Roulette</h1></header>
+      <Navbar />
+      <Route path="/" exact>
+        <h1>Home page</h1>
+      </Route>
+      <Route path="/books" exact>
+        <h1>Books page</h1>
+      </Route>
+      <Route path="/new">
+        <h1>New Book</h1>
+      </Route>
+    </>
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/books">See All Books</Link>
+      <Link to="/new">Add New Book</Link>
+    </nav>
+  )
+}
