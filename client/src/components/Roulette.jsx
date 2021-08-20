91fe637b6ef2f433648d812a30084c2e1e8391a1
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-20 15:18:57 -0500

    roulette!

--- client/src/components/Roulette.jsx
+++ client/src/components/Roulette.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react"
 import Book from "./Book"
 
-export default function Roulette() {
+export default function Roulette(props) {
+  const [book, setBook] = useState({});
+  const {books} = props;
+
+  useEffect(() => {
+    if(books.length===0) return;
+    const bookInd = Math.floor(Math.random() * books.length);
+    setBook(books[bookInd].fields)
+  }, [books])
+
   return(
     <>
       <h1>Roulette</h1>
-      <Book />
+      <Book book={book}/>
     </>
   )
 }
