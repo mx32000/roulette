ca2c59c78a57c980b5f3cca15d0c111af07a1624
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-23 17:39:21 -0500

    expanding and collapsing hamburger menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
 import "../css/Navbar.css"
 
 export default function Navbar() {
+  const [menuStatus, setMenuStatus] = useState("collapsed")
+
+  const toggleMenu = () => {
+    setMenuStatus(menuStatus === "collapsed" ? "expanded" : "collapsed");
+  }
+
   return (
     <header>
       <div className="header">
         <h1>Roulette</h1>
-        <button onClick={() => console.log("hi")}><i className="fas fa-2x fa-bars"></i></button>
+        <button onClick={toggleMenu}><i className="fas fa-2x fa-bars"></i></button>
       </div>
       <div className="nav-container">
-        <nav>
+        <nav className={menuStatus}>
           <Link to="/">Home</Link>
           <Link to="/books">See All Books</Link>
           <Link to="/books/new">Add New Book</Link>
