d3df4b923a0de2b5193c787d7c6e7a1c67f36cb2
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-23 09:48:08 -0500

    flex and menu

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom'
+import '../css/Book.css'
 
 export default function Book(props) {
   const {book} = props;
--- client/src/components/Roulette.jsx
+++ client/src/components/Roulette.jsx
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react"
-import Book from "./Book"
+import { useEffect, useState } from "react";
+import Book from "./Book";
+import "../css/Roulette.css";
 
 export default function Roulette(props) {
   const [book, setBook] = useState({});
   const {books} = props;
 
   const spin = () => {
-    console.log("spin");
     const bookInd = Math.floor(Math.random() * books.length);
-    console.log(books[bookInd].fields.title)
     setBook(books[bookInd]);
   }
 
@@ -17,11 +16,11 @@ export default function Roulette(props) {
   })
 
   return (
-    <>
+    <div className="roulette">
       {
         book.fields ? <Book book={book}/> : <img src="https://res.cloudinary.com/dcdasnmmz/image/upload/v1629514381/Roulette/mystery_tgwugj.png" alt="question mark" />
       }
       <button onClick={spin}>Spin!</button>
-    </>
+    </div>
   )
 }
