3ca4cc2d243990eb977a92b78dc374d22faf0ba5
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-25 13:30:57 -0500

    to top button

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -3,10 +3,9 @@ import "../css/BookList.css"
 
 export default function BookList(props) {
   return (
-    <>
       <div className="book-list">
         {props.books.map((book, index) => <Book book={book} key={index} setActiveTab={() => props.setActiveTab("")} />)}
+        <a href="#" id="to-top">To Top</a>
       </div>
-    </>
   )
 }
