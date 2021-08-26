ecb6babba100bd6b6304d872ff843f43aab5abf4
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-26 14:41:42 -0500

    added search feature to book list

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,13 +1,21 @@
 import Book from "./Book"
 import "../css/BookList.css"
+import { useState } from "react";
 
 export default function BookList(props) {
+  const [search, setSearch] = useState("");
+
   props.setFrom("list");
 
   return (
+    <div className="books-page">
+      <label htmlFor="search">Search</label>
+      <input type="text" name="search" id="search" placeholder="harry potter" value={search} onChange={e => setSearch(e.target.value)}/>
       <div className="book-list">
-        {props.books.map((book, index) => <Book book={book} key={index} setActiveTab={() => props.setActiveTab("")} />)}
+        {props.books.filter(book => book.fields.title.toLowerCase().includes(search.toLowerCase()))
+          .map((book, index) => <Book book={book} key={index} setActiveTab={() => props.setActiveTab("")} />)}
         <a href="#" id="to-top">To Top</a>
       </div>
+    </div>
   )
 }
