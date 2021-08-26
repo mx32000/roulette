efa65e6d63eb4bb1ec6bc6432bd32e5d75ca6050
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-26 15:28:58 -0500

    choose search field

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -4,16 +4,31 @@ import { useEffect, useState } from "react";
 
 export default function BookList(props) {
   const [search, setSearch] = useState("");
+  const [field, setField] = useState("title");
   const {books, setActiveTab, setFrom} = props;
 
   useEffect(() => {setFrom("list")}, [setFrom]);
 
+  const matchesSearch = (book) => {
+    const choices = field === "keyword" ? [book.fields.title, book.fields.author, book.fields.genre, book.fields.summary] : [book.fields[field]];
+    return choices.some(field => field.toLowerCase().includes(search.toLowerCase()));
+  }
+
   return (
     <div className="books-page">
-      <label htmlFor="search">Search</label>
-      <input type="text" name="search" id="search" placeholder="harry potter" value={search} onChange={e => setSearch(e.target.value)}/>
+      <form>
+        <label htmlFor="search">Search:</label>
+        <input type="search" name="search" id="search" value={search} onChange={e => setSearch(e.target.value)}/>
+        <label htmlFor="field">Search by:</label>
+        <select name="field" id="field" onChange={e => setField(e.target.value)}>
+          <option value="title" selected>Title</option>
+          <option value="author">Author</option>
+          <option value="genre">Genre</option>
+          <option value="keyword">Keyword</option>
+        </select>
+      </form>
       <div className="book-list">
-        {books.filter(book => book.fields.title.toLowerCase().includes(search.toLowerCase()))
+        {books.filter(book => matchesSearch(book))
           .map((book, index) => <Book book={book} key={index} setActiveTab={() => setActiveTab("")} />)}
         <a href="#top" id="to-top">To Top</a>
       </div>
