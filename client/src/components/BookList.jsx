f0648156bb6d0a2144051344ea16d014ab414301
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-26 15:01:40 -0500

    move setFrom into useEffect in booklist

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,19 +1,20 @@
 import Book from "./Book"
 import "../css/BookList.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function BookList(props) {
   const [search, setSearch] = useState("");
+  const {books, setActiveTab, setFrom} = props;
 
-  props.setFrom("list");
+  useEffect(() => {setFrom("list")}, [setFrom]);
 
   return (
     <div className="books-page">
       <label htmlFor="search">Search</label>
       <input type="text" name="search" id="search" placeholder="harry potter" value={search} onChange={e => setSearch(e.target.value)}/>
       <div className="book-list">
-        {props.books.filter(book => book.fields.title.toLowerCase().includes(search.toLowerCase()))
-          .map((book, index) => <Book book={book} key={index} setActiveTab={() => props.setActiveTab("")} />)}
+        {books.filter(book => book.fields.title.toLowerCase().includes(search.toLowerCase()))
+          .map((book, index) => <Book book={book} key={index} setActiveTab={() => setActiveTab("")} />)}
         <a href="#top" id="to-top">To Top</a>
       </div>
     </div>
